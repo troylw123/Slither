1b8cc0354895e4e99b3676d046a8a212fe2bb97f
mariaharrell13 <mariaharrell13@gmail.com>
2022-03-23 14:51:52 -0400

    Comment Entity added

---
+++ Slither.Data/Entities/CommentEntity.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Slither.Data.Entities
+{
+    public class CommentEntity
+    {
+        [key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Comment { get; set; }
+
+        [Required]
+        public DateTime DateCreated { get; set; }
+    }
+}
