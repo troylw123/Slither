7e0b614b74e9dec83c49ee797f1801f11c3b001c
Troy Weaver <troylw@gmail.com>
2022-03-24 11:23:00 -0400

    UserController Created

---
+++ Slither.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Slither.Services.User;
+
+namespace Slither.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _service;
+        public UserController(IUserService service)
+        {
+            _service = service;
+        }
+    }
+}
